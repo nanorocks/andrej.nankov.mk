24bbbe47bcb2a10ab59b29f81f2bff381fe25731
nanorocks <andrejnankov@gmail.com>
2020-11-28 12:54:41 +0100

    UPD user seeder

--- api/database/seeders/UsersTableSeeder.php
+++ api/database/seeders/UsersTableSeeder.php
@@ -19,15 +19,20 @@ class UsersTableSeeder extends Seeder
         DB::table(User::TABLE)->insert([
             User::EMAIL => env('DEFAULT_USER_EMAIL'),
             User::NAME => 'Andrej Nankov',
-            User::INTRO => 'Hello, and welcome to my personal website. My name is Andrej Nankov but I introduce myself as nanorocks. I use this nick as better introduction to myself on the internet. This nick is tide connect to my social life and my field of work.
+            User::INTRO => 'Hello, and welcome to my personal website. My name is <strong>Andrej Nankov</strong> but I introduce myself as nanorocks. I use this nick as better introduction to myself on the internet. This nick is tide connect to my social life and my field of work.
                             On this site you can find content most about my work as a software engineer, experience in various fields of IT, open-source, solved problems, how to grow as an engineer and blog posts related to contend that is interesting to read and know.
                             My goal is to be better self promoted to the IT world and to share my knowledge with others.',
             User::SUMMARY => 'Software engineer with a Bachelor\'s degree (Faculty of Computer Science and Engineering - Skopje, Macedonia) experience in various fields in IT with wide skills. Familiar with each layer of the development process and comfortable to work as a member of a team or independently. Worked with different programming languages and frameworks in different fields like web, desktop-mobile software development and system administration. Also, related to open source with interest in leveraging technology to make the world a better place.',
             User::CURRENT_WORK => 'Working in the field of web development & web services, web design, testing, server configuration solutions.',
-            User::TOP_PROGRAMMING_LANGUAGES => 'PHP; JavaScript; Python',
-            USER::QUOTES => '{"Favorite quote": "\"Talk is cheap. Show me the code.\" - Linus Torvalds"}',
-            User::GOALS => 'Generalist with focus on concepts.;Believer that success in career depends on persistence and willingness to learn.;Understanding client requirements and communicating the progress of the project are core values in achieving the project goals.',
-            User::SOC_MEDIA => '{"fb":"https://www.facebook.com/nanorocks", "gh":"https://github.com/nanorocks", "li": "https://www.linkedin.com/in/nanorocks" }',
+            User::TOP_PROGRAMMING_LANGUAGES => 'PHP;JavaScript;Python',
+            USER::QUOTES => json_encode(["Favorite quote" => "\"Talk is cheap. Show me the code.\" - Linus Torvalds"]),
+            User::GOALS => 'Generalist with focus on concepts.;
+            Believer that success in career depends on persistence and willingness to learn.;
+            Understanding client requirements and communicating the progress of the project are core values in achieving the project goals.',
+            User::SOC_MEDIA => json_encode([
+                "facebook" => "https://www.facebook.com/nanorocks",
+                "github"   => "https://github.com/nanorocks",
+                "linkedIn" => "https://www.linkedin.com/in/nanorocks" ]),
             User::PASSWORD => Hash::make('secret')
         ]);
     }
