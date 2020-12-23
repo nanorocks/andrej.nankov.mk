ab674b1a5f4238be9f154e9d0df929302b915eef
nanorocks <andrejnankov@gmail.com>
2020-12-23 17:48:54 +0100

    ADD cors on API

--- api/app/Http/Controllers/ClientSide/PageController.php
+++ api/app/Http/Controllers/ClientSide/PageController.php
@@ -11,10 +11,35 @@ class PageController extends Controller
      * @OA\Get(
      *     path="/cv",
      *     description="Get CV",
-     *     @OA\Contact(
-     *          email="andrejnankov@gmail.com",
-     *          name="Andrej Nankov"
+     *     operationId="loginUser",
+     *     @OA\Parameter(
+     *       name="key",
+     *       required=false,
+     *       in="query",
+     *       description="HMAC_SHARED_API_KEY",
+     *       @OA\Schema(
+     *         type="string"
+     *       )
      *     ),
+     *     @OA\Parameter(
+     *       name="data",
+     *       required=false,
+     *       in="query",
+     *       description="BASE_64_ENCODE_DATA",
+     *       @OA\Schema(
+     *         type="string"
+     *       )
+     *     ),
+     *     @OA\Parameter(
+     *       name="signature",
+     *       required=false,
+     *       in="query",
+     *       description="BASE_64_ENCODE_SIGNATURE",
+     *       @OA\Schema(
+     *         type="string"
+     *       )
+     *     ),
+     *     tags={"ClientSide"},
      *     @OA\Response(response="200", description="Get information from my personal cv")
      * )
      */
