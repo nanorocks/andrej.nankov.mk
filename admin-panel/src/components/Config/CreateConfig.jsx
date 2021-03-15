7fe86f57b03f1e4aa378469177628b4f2d001630
nanorocks <andrejnankov@gmail.com>
2021-03-15 21:58:12 +0100

    WIP create/edit config, post, project

---
+++ admin-panel/src/components/Config/CreateConfig.jsx
@@ -0,0 +1,13 @@
+import { Component, React } from "react";
+
+class CreateConfig extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Create config</div>;
+  }
+}
+
+export default CreateConfig;
---
+++ admin-panel/src/components/Config/UpdateConfig.jsx
@@ -0,0 +1,13 @@
+import { Component, React } from "react";
+
+class UpdateConfig extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Update config</div>;
+  }
+}
+
+export default UpdateConfig;
