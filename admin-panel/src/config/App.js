a7312dbbd4f010ac97c8f852fa0bef758e50d937
nanorocks <andrejnankov@gmail.com>
2021-04-30 12:33:31 +0200

    Hotfix

--- admin-panel/src/config/App.js
+++ admin-panel/src/config/App.js
@@ -1,4 +1,5 @@
 export const app = {
-  BLOG_API_URL: "https://api.nankov.mk/",
+  BLOG_API_URL: "http://homestead.test/",
+  // BLOG_API_URL: "https://api.nankov.mk/",
   ADMIN_PANEL_DOMAIN: "https://andrej.nankov.mk/",
 };
