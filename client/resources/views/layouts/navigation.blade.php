0214f4a1aa660adc361d088b58eca22f8e7de1b2
Andrej Nankov <andrejnankov@gmail.com>
2024-04-08 18:04:16 +0200

    Add landing page

--- client/resources/views/layouts/navigation.blade.php
+++ client/resources/views/layouts/navigation.blade.php
@@ -45,6 +45,13 @@
                     </a>
                 </div>
 
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <a href="{{ route('home') }}" target="_blank"
+                        class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                        {{ __('Landing page') }}
+                    </a>
+                </div>
+
             </div>
 
             <!-- Settings Dropdown -->
--- client/resources/views/livewire/layout/navigation.blade.php
+++ client/resources/views/livewire/layout/navigation.blade.php
@@ -62,6 +62,14 @@ new class extends Component {
                     </a>
                 </div>
 
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <a href="{{ route('home') }}" target="_blank"
+                        class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out">
+
+                        {{ __('Landing page') }}
+                    </a>
+                </div>
+
             </div>
 
             <!-- Settings Dropdown -->
@@ -105,6 +113,10 @@ new class extends Component {
                             class="block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 dark:hover:border-gray-600 focus:outline-none focus:text-gray-800 dark:focus:text-gray-200 focus:bg-gray-50 dark:focus:bg-gray-700 focus:border-gray-300 dark:focus:border-gray-600 transition duration-150 ease-in-out">
                             {{ __('Google Analytics') }}
                         </a>
+
+                        <x-dropdown-link :href="route('home')" wire:navigate>
+                            {{ __('Landing page') }}
+                        </x-dropdown-link>
 
 
                         <!-- Authentication -->
@@ -171,6 +183,9 @@ new class extends Component {
                     {{ __('Google Analytics') }}
                 </a>
 
+                <x-responsive-nav-link :href="route('home')" wire:navigate>
+                    {{ __('Landing page') }}
+                </x-responsive-nav-link>
 
                 <!-- Authentication -->
                 <button wire:click="logout" class="w-full text-start">
