fb6b9948405b8238046fc64528412ed7798de9f3
Andrej Nankov <andrejnankov@gmail.com>
2024-04-15 14:50:57 +0200

    Update dashboard.blade.php

--- client/resources/views/dashboard.blade.php
+++ client/resources/views/dashboard.blade.php
@@ -18,7 +18,7 @@
                     {{ __("You're logged in! Nothing here this is only SSO Table.") }}
                     <a class="underline" href="https://oauth2.thephpleague.com/authorization-server/which-grant/"
                         target="_blank">Supported OAuth2 clients?</a>
-                    <div>
+                    <div style="padding-bottom: 9rem;">
                         @livewire('o-auth-clients-data-table')
                     </div>
                 </div>
