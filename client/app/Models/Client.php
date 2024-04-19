2593021605fc22ad422a2a43fdedc28c1ede2ed7
Andrej Nankov <andrejnankov@gmail.com>
2024-04-19 02:10:52 +0200

    Hotfix client

--- client/app/Models/Client.php
+++ client/app/Models/Client.php
@@ -7,4 +7,9 @@ use Laravel\Passport\Client as PassportClient;
 
 class Client extends PassportClient
 {
+
+    public function skipsAuthorization(): bool
+    {
+        return $this->firstParty();
+    }
 }
