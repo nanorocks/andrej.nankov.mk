d81e70ca882565e29335949cd570e479a8d1198a
Andrej Nankov <andrejnankov@gmail.com>
2024-05-11 23:40:15 +0200

    Add optimize command instead of cpu

--- client/app/Providers/HealthCheckServiceProvider.php
+++ client/app/Providers/HealthCheckServiceProvider.php
@@ -5,11 +5,11 @@ namespace App\Providers;
 use Spatie\Health\Facades\Health;
 use Illuminate\Support\ServiceProvider;
 use Spatie\Health\Checks\Checks\DatabaseCheck;
-use Spatie\Health\Checks\Checks\EnvironmentCheck;
-use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
-use Spatie\CpuLoadHealthCheck\CpuLoadCheck;
-use Spatie\SecurityAdvisoriesHealthCheck\SecurityAdvisoriesCheck;
 use Spatie\Health\Checks\Checks\ScheduleCheck;
+use Spatie\Health\Checks\Checks\EnvironmentCheck;
+use Spatie\Health\Checks\Checks\OptimizedAppCheck;
+use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
+use Spatie\SecurityAdvisoriesHealthCheck\SecurityAdvisoriesCheck;
 
 class HealthCheckServiceProvider extends ServiceProvider
 {
@@ -22,9 +22,7 @@ class HealthCheckServiceProvider extends ServiceProvider
             UsedDiskSpaceCheck::new(),
             DatabaseCheck::new(),
             EnvironmentCheck::new(),
-            CpuLoadCheck::new()
-                ->failWhenLoadIsHigherInTheLast5Minutes(2.0)
-                ->failWhenLoadIsHigherInTheLast15Minutes(1.5),
+            OptimizedAppCheck::new(),
             SecurityAdvisoriesCheck::new(),
             ScheduleCheck::new(),
         ]);
