7731dbdfa73dac44b0c7b9c401b07bf6a441b0ea
nanorocks <andrejnankov@gmail.com>
2024-12-19 20:08:37 +0100

    Add clean up code + new project

--- client/resources/views/components/dark/quotes.blade.php
+++
@@ -1,9 +0,0 @@
-<section id="gtco-counter" class="overlay bg-fixed">
-    <div class="container">
-        <div class="section-content">
-            <div class="row text-center py-5">
-                <h2 style="font-weight: 500; color:white;">{!! $quotes[rand(0, 9)]->title->rendered !!}</h2>
-            </div>
-        </div>
-    </div>
-</section>
