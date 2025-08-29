68dd597e3ed08f49bfcdac6686170d9bd23b520f
nanorocks <andrejnankov@gmail.com>
2025-08-29 22:30:34 +0200

    Add missing check for production

--- website/app/Models/User.php
+++ website/app/Models/User.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
@@ -19,8 +21,22 @@ class User extends Authenticatable
      */
     protected $fillable = ['name', 'avatar', 'email'];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
+
+    /**
+     * Determine if the user can access the Filament admin panel.
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // You can add more logic here based on roles or permissions
+        return str_starts_with($this->email, 'andrejnankov') && $this->hasVerifiedEmail();
+    }
 }
